8047b271ab6b7aa437b6584a37a4d462c1c40898
jasingh784 <48878742+jasingh784@users.noreply.github.com>
2021-06-13 13:14:45 -0700

    working on reply screen

---
+++ components/CreatePost.js
@@ -0,0 +1,163 @@
+import React, {useEffect, useState} from 'react'
+import { SafeAreaView, TextInput, StyleSheet, ActivityIndicator, StatusBar, Pressable, Text, Image, TouchableWithoutFeedback, Keyboard, View } from 'react-native'
+import MyButton from '../components/MyButton';
+import PostHeader from '../components/PostComponents/PostHeader'
+import { createPost } from '../utils/api'
+import DrawerHeader from '../components/DrawerHeader';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import * as ImagePicker from 'expo-image-picker';
+import { Ionicons } from '@expo/vector-icons';
+import { firebase } from '../firebase/config';
+
+
+function CreatePost() {
+
+    const [postText, setPostText] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
+    const [loggedInUser, setLoggedInUser] = useState('');
+    const [image, setImage] = useState(null);
+    const [isPosting, setisPosting] = useState(false);
+
+    useEffect( () => {
+        async function getUserId() {
+            const userID = await AsyncStorage.getItem('userid'); 
+            setLoggedInUser(userID);
+            setIsLoading(false);
+        }
+        getUserId();
+    }, []);
+
+    const uploadImage = async() => {
+        const storageRef = firebase.storage().ref();
+        const response = await fetch(image);
+        const blob = await response.blob();
+
+        //push to path
+        const snapshot = await storageRef.child('images/' + Date.now()).put(blob)
+
+        console.log('Uploaded', snapshot.totalBytes, 'bytes');
+        console.log('file metadata:', snapshot.metadata);
+        //get download url
+        
+        return await snapshot.ref.getDownloadURL()
+
+    }
+
+    const submitPost = async() => {
+        setisPosting(true);
+        let mediaUrl = null;
+        if(image) {
+            mediaUrl = await uploadImage();
+            console.log("media url", mediaUrl);
+        }
+        const postId = await createPost({postText, mediaUrl});
+        if(postId != 0) {
+            setPostText('');
+            setImage(null);
+            setisPosting(false);
+            navigation.navigate("Profile")
+        } else {
+            setisPosting(false);
+            Alert.alert(
+                "Failed",
+                "Unable to create post. Please try again",
+                [
+                  {
+                    text: 'OK'
+                  }
+                ]
+              )
+        }
+    }
+
+    const addImage = async() => {
+        
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if(status !== 'granted') {
+            alert('Sorry, we need camera roll permissions')
+            return;
+        }
+
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: false,
+        })
+
+        if(!result.cancelled) {
+            setImage(result.uri);
+        }
+    }
+
+    const removeImage = () => {
+        setImage(null);
+    }
+
+    return (
+        
+            
+            <View style={styles.container}>
+            
+
+            {isLoading ? <ActivityIndicator color="#000000" size="large"/> : (
+                <PostHeader author={loggedInUser}/>
+            )}
+
+            {isPosting ? <ActivityIndicator color="#000000" size="large"/> : (
+            <>
+            <TextInput 
+                style={styles.input}
+                onChangeText={setPostText}
+                value={postText}
+                placeholder="Share your thoughts with the world"
+                multiline={true}
+                maxLength={128}
+            />
+
+            {image && <Image source={{ uri: image }} style={styles.image} />}
+
+            {!image && (
+            <Pressable onPress={addImage} style={{flexDirection: 'row', alignItems: 'center', marginVertical: 15}}>
+                <Ionicons name="image-outline" size={30} color="black" style={styles.imageIcon}/>
+                <Text>Add Image</Text>
+            </Pressable> )}
+
+            {image && (
+            <Pressable onPress={removeImage} style={{flexDirection: 'row', alignItems: 'center', marginVertical: 15}}>
+                <Ionicons name="remove-circle-outline" size={30} color="black" style={styles.imageIcon}/>
+                <Text>Remove Image</Text>
+            </Pressable> )}
+            
+            <MyButton 
+                title='Post'
+                onPress={submitPost}
+            />
+            </>
+            )}
+            </View>
+            
+        
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+    },  
+    input: {
+        marginHorizontal: 10,
+        fontSize: 18,
+        marginVertical: 20,
+    },
+    imageIcon: {
+        marginHorizontal: 10,
+    },
+    image: {
+        width: 200,
+        height: 200,
+        alignSelf: 'center',
+    }
+})
+
+export default CreatePost
--- screens/SinglePostScreen.js
+++ screens/SinglePostScreen.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet, Modal, Pressable, TextInput, Keyboard } from 'react-native'
 import Post from '../components/Post';
 import { getOnePost } from '../utils/api';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Button } from 'react-native-paper';
+import CreatePost from '../components/CreatePost';
 
 
 export default function SinglePostScreen({ navigation, route }) {
     //get params
     const {postId} = route.params;
     const [post, setPost] = useState(null)
+    const [modalVisible, setModalVisible] = useState(false)
+    const [replyText, setReplyText] = useState('')
 
     const getPost = async() => {
         let response = await getOnePost(postId);
@@ -24,12 +27,39 @@ export default function SinglePostScreen({ navigation, route }) {
         <SafeAreaView style={{flex: 1,}}>
             {!post ? <Text>Opps. Something went wrong. :(</Text> : (
                 <>
+                    
                     <Post postText={post.postText} author={post.author} mediaUrl={post.mediaUrl} />
-                    <Button mode="contianed" style={styles.replyButton} onPress={() => console.log('pressed')}>
+                    <Button mode="contianed" style={styles.replyButton} onPress={() => setModalVisible(true)}>
                         <MaterialCommunityIcons name="reply-outline" size={32} color="white" />
                     </Button>
+                    
                 </>
             )}
+            <Modal
+                animationType="slide"
+                visible={modalVisible}
+                transparent={true}
+                onRequestClose={() => {
+                    setModalVisible(false);
+                }}
+            >
+                <Pressable style={{flex: 1}} onPress={Keyboard.dismiss}>
+                    <View style={styles.replyModal}>
+                        
+                        <Button 
+                            mode="text" 
+                            onPress={() => setModalVisible(false)} 
+                            color='black'
+                            style={styles.modalClose}
+                        >
+                            <MaterialCommunityIcons name="close" size={24} color="black" />
+                        </Button>
+                        <Text>Replying to:</Text>
+                        <CreatePost />
+                        
+                    </View>
+                </Pressable>
+            </Modal>
         </SafeAreaView>
     )
 }
@@ -44,5 +74,25 @@ const styles = StyleSheet.create({
         position: 'absolute',
         bottom: 70,
         right: 25,
+    },
+    modalClose: {
+        alignSelf: 'flex-end'
+    },  
+    replyModal: {
+        height: '60%',
+        margin: 20,
+        marginTop: 'auto',
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 15,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
     }
 })
